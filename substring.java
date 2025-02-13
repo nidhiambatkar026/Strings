2417e1f8f916d33cd105c0ec82845e9b2fa0ed18
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-02-13 12:13:38 +0530

    substring function added

---
+++ substring.java
@@ -0,0 +1,7 @@
+public class substring {
+    public static void main(String args[]){
+        String sentence = "My name is Nidhi";
+        String print = sentence.substring(11, sentence.length());
+        System.out.println(print);
+    }
+}
