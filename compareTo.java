a6e703b20b7f8694bad9af95ae6fc675135c7b5c
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-02-13 12:00:18 +0530

    compareTo function added

---
+++ compareTo.java
@@ -0,0 +1,13 @@
+public class compareTo {
+    public static void main(String args[]){
+        String name1 = "Nidhi";
+        String name2 = "nidhi";
+
+        if(name1.compareTo(name2) == 0){
+              System.out.println("Strings are equal");
+        }
+        else{
+            System.out.println("Strings are not equal");
+        }
+    }
+}
