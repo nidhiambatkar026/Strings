fe0a9e0bda8d6474666f0d63aaeaf176be154884
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-02-13 10:14:24 +0530

    concatenation and length function used

---
+++ functions.java
@@ -0,0 +1,10 @@
+public class functions {
+    public static void main(String args[]){
+        String firstName = "Nidhi";
+        String LastName = "Ambatkar";
+        String FullName = firstName + LastName;
+        System.out.println(FullName);
+        //length
+        System.out.println(FullName.length());
+    }
+}
